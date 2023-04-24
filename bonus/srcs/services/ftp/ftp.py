346de02e89984f5d506fc2d61bca98047f8f61f3
Hassan Sarhan <hassanAsarhan@outlook.com>
2023-04-24 20:59:25 +0400

    FTP works

---
+++ bonus/srcs/services/ftp/ftp.py
@@ -0,0 +1,35 @@
+import os
+
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+def main():
+    # Instantiate a dummy authorizer for managing 'virtual' users
+    authorizer = DummyAuthorizer()
+
+    # Define a new user having full r/w permissions and a read-only
+    # anonymous user
+    authorizer.add_user('mr_file', os.environ['FTP_PASS'], '/var/www/html/wordpress', perm='elradfmwMT')
+    authorizer.add_anonymous(os.getcwd())
+
+    # Instantiate FTP handler class
+    handler = FTPHandler
+    handler.authorizer = authorizer
+
+    # Define a customized banner (string returned when client connects)
+    handler.banner = "CLIENT CONNECTED"
+
+    # Instantiate FTP server class and listen on 0.0.0.0:2121
+    address = ('', 21)
+    server = FTPServer(address, handler)
+
+    # set a limit for connections
+    server.max_cons = 256
+    server.max_cons_per_ip = 5
+
+    # start ftp server
+    server.serve_forever()
+
+if __name__ == '__main__':
+    main()
